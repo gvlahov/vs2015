d18700f43bb9b1be74dcb1d6686278d7d15233e7
gvlahov <grigor.vlahov@gmail.com>
2016-12-21 17:14:58 +0200

    Add rest packages

---
+++ WebApplicationMVCEmpty/WebApplicationMVCEmpty/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationMVCEmpty.Startup))]
+namespace WebApplicationMVCEmpty
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
